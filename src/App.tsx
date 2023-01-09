553bd206d81977cbb9c8cd7ae014cc695ebefd1a
Olesya <olesyam.miller@gmail.com>
2023-01-08 23:40:32 -0400

    first commit, initial setup

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { LoginForm } from './components/auth/login-form/login-form.component';
+
+function App() {
+  return (
+    <LoginForm />
+  );
+}
+
+export default App;
