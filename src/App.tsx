1e238888642234052acef69808b60c538b10e7e0
Olesya <olesyam.miller@gmail.com>
2023-01-09 00:05:51 -0400

    added html and styles to login form component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import "./index.css";
 import { LoginForm } from './components/auth/login-form/login-form.component';
 
 function App() {
--- src/components/auth/login-form/login-form.component.tsx
+++ src/components/auth/login-form/login-form.component.tsx
@@ -1,8 +1,18 @@
+import { Button, TextField } from '@mui/material';
 import * as React from 'react';
 
 const LoginForm: React.FC = () => {
     return (
-        <></>
+        <div className="flex justify-center items-center flex-col h-screen gap-8">
+            <h1>Cryptostats</h1>
+            <div className="flex flex-col gap-2">
+                <TextField label="Email" className="w-80" type="email" required />
+                <TextField label="Password" type="password" className="w-80" required />
+            </div>
+            <Button variant="contained" className="w-80">
+                <span className="p-1">Login</span>
+            </Button>
+        </div>
     )
 }
 
