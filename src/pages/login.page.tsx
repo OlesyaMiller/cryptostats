b213ca42c1769c445005f755511a3234600d312a
Olesya <olesyam.miller@gmail.com>
2023-01-10 13:50:52 -0400

    set up authentication

---
+++ src/pages/login.page.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { LoginForm } from '../components/auth/login-form/login-form.component';
+
+const LoginPage: React.FC = () => {
+    return (
+        <LoginForm />
+    )
+}
+
+export { LoginPage }
