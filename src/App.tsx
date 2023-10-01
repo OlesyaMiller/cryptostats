864bef71032eadbce747f490c34d05c4c231ed30
Olesya <olesyam.miller@gmail.com>
2023-10-01 11:50:34 -0400

    add UI funtionality

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import "./index.css";
 import { createTheme, ThemeProvider } from '@mui/material';
 import { Routes } from "./routes/routes";
+import { useGetUserQuery } from './apis/users.api';
+import { useAppDispatch } from './app/hooks';
+import { useNavigate } from 'react-router';
 
 const darkTheme = createTheme({
   palette: {
@@ -10,6 +13,10 @@ const darkTheme = createTheme({
 })
 
 function App() {
+  const { data: user } = useGetUserQuery(undefined);
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+  
   return (
     <ThemeProvider theme={darkTheme}>
       <Routes />
--- src/pages/home.page.tsx
+++ src/pages/home.page.tsx
@@ -1,8 +1,19 @@
+import { Button } from '@mui/material';
 import * as React from 'react';
 
 const HomePage: React.FC = () => {
+    const handleConnectCoinbase = () => {
+        if (process.env.REACT_APP_COINBASE_AUTH_URL) {
+            window.location.href = process.env.REACT_APP_COINBASE_AUTH_URL;
+        }
+    }
+
     return (
-        <></>
+        <div className='flex justify-center items-center flex-col h-screen'>
+            <Button variant='contained' size='large' onClick={handleConnectCoinbase}>
+                Connect Coinbase
+            </Button>
+        </div>
     )
 }
 
