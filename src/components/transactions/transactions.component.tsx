4e63266a9860627b73b00a86fbde4dd58a645a25
Olesya <olesyam.miller@gmail.com>
2024-10-08 21:45:25 -0400

    add transactions component

---
+++ src/components/transactions/transactions.component.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+interface Props {
+}
+
+const Transactions: React.FC<Props> = () => {
+    return (
+        <div>
+            Transactions
+        </div>
+    );
+};
+
+export { Transactions };
