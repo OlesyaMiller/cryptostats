d8ddfcc6e462b17e92e7876a25aaea70771fd5e5
Olesya <olesyam.miller@gmail.com>
2024-10-16 20:46:23 -0400

    update app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,31 @@
-import React from 'react';
+import { useEffect } from "react";
+import { createTheme, ThemeProvider } from "@mui/material";
+
 import "./index.css";
-import { createTheme, ThemeProvider } from '@mui/material';
 import { Routes } from "./routes/routes";
+import { useGetUserQuery } from "./apis/users.api";
+import { useAppDispatch } from "./app/hooks";
+import { useNavigate } from "react-router";
+import { setAuthState } from "./slices/auth.slice";
 
 const darkTheme = createTheme({
   palette: {
-    mode: "dark"
-  }
-})
+    mode: "dark",
+  },
+});
 
 function App() {
+  const { data: user } = useGetUserQuery(undefined);
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (user) {
+      dispatch(setAuthState({ user }));
+      navigate("/");
+    }
+  }, [user, dispatch, navigate]);
+
   return (
     <ThemeProvider theme={darkTheme}>
       <Routes />
